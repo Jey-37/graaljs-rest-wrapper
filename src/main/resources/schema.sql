92ccf2f7b9e8725034811b68f9a73ca7d3a7275d
Vasil <70105028+Jey-37@users.noreply.github.com>
2023-06-14 17:38:23 +0300

    Schema was changed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-create table if not exists Script (
+create table if not exists Scripts (
     id int auto_increment primary key,
     body varchar(5000) not null,
     status enum('queued', 'executing', 'completed', 'failed', 'interrupted') not null default 'queued',
     output varchar(5000),
-    errors varchar(5000),
+    errors varchar(2000),
     sched_time timestamp,
-    exec_time timestamp
+    exec_time INT
 );
